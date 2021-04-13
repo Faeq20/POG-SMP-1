1fee99b96598c51fa6081410b853843bc7067b65
Faeq20 <81888599+Faeq20@users.noreply.github.com>
2021-04-13 13:11:02 +0700

    first commit

--- hosting.js
+++ hosting.js
@@ -1,2 +1,11 @@
 const express = require("express")
-const app = express
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Minecraft Server")
+})
+
+// creating a web server :: 3000
+app.listen(3000, () => {
+    console.log("Starting the server")
+})
